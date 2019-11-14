b3df85af38900c6b524706f5be4230510acec0db
HaroldMac <harold.macdonald52@gmail.com>
2019-11-14 01:53:01 -0700

    fixed menu calling appi too many times bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FauxPho';
+
+  pastPageYOffsetValue: number = 0;
+
+  ngOnInit() {
+    window.addEventListener('scroll', this.scroll, true); 
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('scroll', this.scroll, true);
+  }
+
+  scroll = (event: any): void => {
+    var topNav = document.querySelector("#topNav-container");
+    if (window.pageYOffset > this.pastPageYOffsetValue) {
+      topNav.classList.add("hide");
+    } else {  
+      topNav.classList.remove("hide");
+    }
+    this.pastPageYOffsetValue = window.pageYOffset;
+  };
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,57 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { NavTopComponent } from './nav-top/nav-top.component';
+import { MenuComponent } from './menu/menu.component';
+import { ProductComponent } from './product/product.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckOrderStatusComponent } from './check-order-status/check-order-status.component';
+import { CheckOrderComponent } from './check-order/check-order.component';
+import { NavSideComponent } from './nav-side/nav-side.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { OrderSubmittedComponent } from './order-submitted/order-submitted.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    MainComponent,
+    NavTopComponent,
+    MenuComponent,
+    ProductComponent,
+    ShoppingCartComponent,
+    CheckOrderStatusComponent,
+    CheckOrderComponent,
+    NavSideComponent,
+    OrderSubmittedComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      { path: 'Home', component: MainComponent },
+      { path: 'Menu', component: MenuComponent },
+      { path: 'CheckOrder', component: CheckOrderComponent },
+      { path: 'CheckOrderStatus', component: CheckOrderStatusComponent },
+      { path: 'ShoppingCart', component: ShoppingCartComponent },
+      { path: 'OrderSubmitted', component: OrderSubmittedComponent },
+      { path: '', redirectTo: 'Home', pathMatch: 'full' },
+      { path: '**', redirectTo: 'Home', pathMatch: 'full' }
+
+      
+    ]),
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
